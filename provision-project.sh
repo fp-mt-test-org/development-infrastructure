48d933f498fb7c169c6807f907dc7e17447e9b82
Matthew Thomas <matthewthomas@flexport.com>
2021-06-28 18:17:49 -0700

    fixing path

--- provision-project.sh
+++ provision-project.sh
@@ -20,32 +20,26 @@ if [[ "${artifactory_base_url:=}" == "" ]]; then
     exit 1
 fi
 
-echo "Inputs:"
-echo "artifactory_base_url: ${artifactory_base_url}"
-echo "project_name: ${project_name}"
+terraform_path="$(realpath ./terraform)"
+
+echo "terraform_path: $terraform_path"
+
+docker run \
+    -w /tform \
+    --volume "${terraform_path}:/tform" \
+    hashicorp/terraform plan \
+    -var="github_repo_name=${project_name}" \
+    -var="artifactory_base_url=${artifactory_base_url}" \
+    -out provision.plan
+
 echo
-echo "Creating..."
-echo 
+echo "Plan created, applying..."
 
-(
-    cd terraform
+docker run \
+    -w /tform \
+    --volume "${terraform_path}:/tform" \
+    hashicorp/terraform apply \
+    provision.plan
 
-    docker run \
-        -w /tform \
-        --volume ~/git/development-infrastructure/terraform:/tform \
-        hashicorp/terraform plan \
-        -var="github_repo_name=${project_name}" \
-        -var="artifactory_base_url=${artifactory_base_url}" \
-        -out provision.plan
-
-    echo
-    echo "Plan created, applying..."
-
-    docker run \
-        -w /tform \
-        --volume ~/git/development-infrastructure/terraform:/tform \
-        hashicorp/terraform apply \
-        provision.plan
-)
 
 echo "${project_name} set up completed successfully!"
