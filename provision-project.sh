43b19bd28853532af2cb9c4c55a97c0bc964cb7d
Matthew Thomas <matthewthomas@flexport.com>
2021-06-28 20:56:34 -0700

    dfsa

--- provision-project.sh
+++ provision-project.sh
@@ -16,7 +16,8 @@ fi
 
 validate "project_name" 
 
-./provision-artifactory.sh
+
+# ./provision-artifactory.sh # Commenting out for now to speed up testing of k8s.
 ./provision-kubernetes.sh
 
 echo "${project_name} set up completed successfully!"
