2276b1d15be1302f2f2784ed42e4909812e6ff4d
Matthew Thomas <matthewthomas@flexport.com>
2021-06-30 10:12:17 -0700

    Bring it back!

--- provision-project.sh
+++ provision-project.sh
@@ -16,7 +16,7 @@ fi
 
 validate "project_name" 
 
-# ./provision-artifactory.sh # Commenting out for now to speed up testing of k8s.
+./provision-artifactory.sh
 ./provision-kubernetes.sh
 
 echo "${project_name} set up completed successfully!"
